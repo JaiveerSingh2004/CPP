9ae490d96168ee3a506b5858d3412992a9dc057f
Jaiveer Singh <144548472+JaiveerSingh2004@users.noreply.github.com>
2024-01-15 22:53:47 +0530

    Create Day6.cpp

---
+++ Data Structures and Algorithms/All Codes/HW/Day6.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// IF-Else H.W.
+
+/*
+// 1. Two numbers are given, print the bigger number, It is given that both numbers can’t be the same.
+
+    int a, b;
+    cout<<"Enter the values of a and b (space seperated): "<<endl;
+    cin>>a;
+    cin>>b;
+    if (a>b)
+    {
+        cout<<a<<" is greater than "<<b<<endl;
+    }
+    else
+    {
+        cout<<b<<" is greater than "<<a<<endl;
+    }
+*/
+
+/*
+// 2. Age of a person is given, print Adult if his/her age is greater than 18, otherwise print Teenager.
+
+    int age;
+    cout<<"Enter the age: "<<endl;
+    cin>>age;
+    if (age>18)
+    {
+        cout<<"Adult";
+    }
+    else if (age<18 and age>12)
+    {
+        cout<<"Teenager";
+    }
+*/
+
+/*
+// 3. Take a number in input (ex n) and print the corresponding month to it. Ex: for n=1, print january, n=2, print feburary like this you need to give output. It is given that n will be greater than 0 and less than 13.
+
+    int mon;
+    cout<<"Enter: "<<endl;
+    cin>>mon;
+    if (mon == 1)
+    {
+        cout<<"Janaury";
+    }
+    else if(mon == 2)
+    {
+        cout<<"Feb";
+    }
+*/
+
+/*
+// 4. Ticket Price Calculator: Create a program that asks the user for their age and checks if they qualify for a discounted ticket price (e.g., under 12 and over 65 get discounts), If they are eligible print “YES” else “NO”.
+
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    if (age<=12 and age>=65)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+*/
+
+// Loop Questions
+
+/*
+// 1. Print “India will win the World Cup 2023”, 20 times.
+    for (int i = 1; i<=20; i++)
+    {
+        cout<<"India will win the World Cup 2023."<<endl;
+    }
+*/
+
+/*
+// 2. Print all Odd numbers from 1 to n, take n as an input from the user.
+    int n;
+    cout<<"Enter: ";
+    cin>>n;
+
+    for (int i=1; i<=n; i++)
+    {
+        if (i%2==1)
+        {
+            cout<<i<<endl;
+        }
+    }
+*/
+
+/*
+// 3. Print all numbers from 1 to n, which is divisible by 4. Take n as an input from the user.
+    int n, i;
+    cout<<"Enter: ";
+    cin>>n;
+
+    for (i=1; i<=n; i++)
+    {
+        if (i % 4 == 0)
+        {
+            cout<<i<<endl;
+        }
+    }
+*/
+}
